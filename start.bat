42270fd4408c10fb5e58c64c4256165b5a3d9ec1
mineorbit <maximilian.stillger@arcor.de>
2024-02-09 04:53:21 +0100

    added files

---
+++ start.bat
@@ -0,0 +1,12 @@
+cd data
+
+cd lobby
+start java -jar ../../bin/paper-1.20.4-409.jar
+cd ..
+cd pvp
+start java -jar ../../bin/paper-1.20.4-409.jar
+cd ..
+cd proxy
+start java -jar ../../bin/velocity-3.3.0-SNAPSHOT-351.jar
+
+pause
